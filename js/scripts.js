034ff867b258b5145fdeb84bcea319992153f6b9
Johnny duverseau and Nick Gallo <johnny+nick@>
2020-01-15 16:39:03 -0800

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,45 @@
-$(document).ready(function() {
-    $("#formOne").submit(function(event) {
-      var person1Input = $("input#person1").val();
-      var person2Input = $("input#person2").val();
-      var animalInput= $("input#animal").val();
-      var exclamationInput = $("input#exclamation").val();
-      var verbInput = $("input#verb").val();
-      var nounInput = $("input#noun").val();
-  
-      $(".person1").text(person1Input);
-      $(".person2").text(person2Input);
-      $(".animal").text(animalInput);
-      $(".exclamation").text(exclamationInput);
-      $(".verb").text(verbInput);
-      $(".noun").text(nounInput);
-  
-      $("#story").show();
-  
-      event.preventDefault();
-    });
-  });
+//$(document).ready(function() {
+
+
+
+  /* $("#formOne").submit(function(event){
+   var person1Input = $("input#person1").val();  
+   var person2Input = $("input#person2").val();
+   var animalInput = $("input#animal").val();
+   var exclamationInput = $("input#exclamation").val();
+   var verbInput = $("input#verb").val();
+   var nounInput = $("input#noun").val();
+
+   $(".person1").append("blah blah");
+   $(".person2").append("blah blah");
+   $(".animal").append("blah blah");
+   $("exclamation").append("blah blah");
+   $(".verb").append("blah blah");
+   $(".noun").append("blah blah");
+
+   $("#story").show();
+
+   event.preventDefault();
+
+   });
+});*/
+
+
+// var dans = "Johnny";
+// function amour() {
+//    alert(dans);
+//    dans="hahahahah";
+// }
+
+// amour();
+// alert(dans);
+
+
+var school = "Epicodus";
+function nickName() {
+alert(nickname);
+} 
+nickName()
+alert(nickName);
+
+
